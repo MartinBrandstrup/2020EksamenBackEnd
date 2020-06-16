0b8e21bacd29ef0e088e7fb1efa1507ed3b9fbf8
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-06-16 16:43:24 +0200

    Set up Test class with entities and added a few tests

--- src/test/java/facades/MasterFacadeTest.java
+++ src/test/java/facades/MasterFacadeTest.java
@@ -1,8 +1,12 @@
 package facades;
 
 import entities.FoodItem;
+import entities.Recipe;
 import utils.EMF_Creator;
 import entities.Storage;
+import entities.WeekMenu;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.AfterAll;
@@ -59,10 +63,22 @@ public class MasterFacadeTest
 //        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
     }
 
-    FoodItem testFI1 = new FoodItem("Kartofler", 1000);
+    FoodItem testFI1 = new FoodItem("Kartofler", 1000); //Pris pr 1000gram
     FoodItem testFI2 = new FoodItem("Løg", 900);
-    Storage testS1 = new Storage(4, testFI1);
-
+    Storage testS1 = new Storage(60, testFI1); //Vægt i gram
+    
+    Recipe testR1 = new Recipe("Grøntsagssuppe", 30, "Lav suppen"); //Tid i minutter
+    Recipe testR2 = new Recipe("Bagte Bagekartofler", 30, "Bag dem");
+    Recipe testR3 = new Recipe("Råstegte Kartofler", 30, "Steg dem");
+    Recipe testR4 = new Recipe("Gratinerede Kartofler", 30, "Gratiner dem");
+    Recipe testR5 = new Recipe("Hasselback Kartofler", 30, "Do the thing");
+    Recipe testR6 = new Recipe("Pomfritter", 30, "Frituresteg dem");
+    Recipe testR7 = new Recipe("Rå Kartofler", 30, "Don't do the thing");
+    
+    Set<Recipe> potatoes = new HashSet();
+    
+    WeekMenu menu = new WeekMenu(potatoes);
+   
     // Setup the DataBase in a known state BEFORE EACH TEST
     @BeforeEach
     public void setUp()
@@ -71,16 +87,39 @@ public class MasterFacadeTest
         try
         {
             em.getTransaction().begin();
-            em.createNamedQuery("Storage.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Recipe_FoodItem.deleteAllRows").executeUpdate();
             em.createNamedQuery("FoodItem.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Storage.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Recipe.deleteAllRows").executeUpdate();
 
-//            FoodItem testFI1 = new FoodItem("Kartofler", 1000);
-//            FoodItem testFI2 = new FoodItem("Løg", 900);
-//            Storage testS1 = new Storage(4, testFI1);
-
+            testR1.addFoodItem(testFI1, 12000); //Vægt i gram
+            testR1.addFoodItem(testFI2, 20000);
+            
+            testR2.addFoodItem(testFI1, 16000);
+            testR3.addFoodItem(testFI1, 12000);
+            testR4.addFoodItem(testFI1, 12000);
+            testR5.addFoodItem(testFI1, 12000);
+            testR6.addFoodItem(testFI1, 8000);
+            testR7.addFoodItem(testFI1, 12000);
+            potatoes.add(testR1);
+            potatoes.add(testR2);
+            potatoes.add(testR3);
+            potatoes.add(testR4);
+            potatoes.add(testR5);
+            potatoes.add(testR6);
+            potatoes.add(testR7);
+            
+            
             em.persist(testFI1);
             em.persist(testFI2);
             em.persist(testS1);
+            em.persist(testR1);
+            em.persist(testR2);
+            em.persist(testR3);
+            em.persist(testR4);
+            em.persist(testR5);
+            em.persist(testR6);
+            em.persist(testR7);
 
             em.getTransaction().commit();
         }
@@ -121,32 +160,51 @@ public class MasterFacadeTest
     {
         System.out.println("getStorageCount");
         
-        long expResult = 0;
+        long expResult = 1;
         long result = facade.getStorageCount();
         assertEquals(expResult, result);
     }
 
     /**
      * Test of persistFoodItemToStorage method, of class MasterFacade.
+     * When providing existing FoodItem.
      */
     @Test
-    public void testPersistFoodItemToStorage()
+    public void testExistingPersistFoodItemToStorage()
     {
         System.out.println("persistFoodItemToStorage");
         
         String itemName = "Kartofler";
-        long itemAmount = 5;
-        long itemPrice = 4444;
+        long itemAmount = 60;
+        long itemPrice = 200;
         
         long expResult = testS1.getFoodItemAmount() + itemAmount;
-        long result = facade.persistFoodItemToStorage(itemName, itemAmount, itemPrice).getFoodItemAmount();
-        assertEquals(expResult, result);
-        assertEquals(2, facade.getFoodItemCount());
-        assertEquals(1, facade.getStorageCount());
+        long result = facade.persistUpdateFoodItemToStorage(itemName, itemAmount, itemPrice)
+                .getFoodItemAmount();
         
-        facade.persistFoodItemToStorage("Hvidløg", itemAmount, itemPrice);
-        assertEquals(3, facade.getFoodItemCount());
-        assertEquals(2, facade.getStorageCount());
+        assertEquals(expResult, result);
     }
+    
+//    /**
+//     * Test of persistFoodItemToStorage method, of class MasterFacade.
+//     * When providing non-existing FoodItem.
+//     */
+//    @Test
+//    public void testNewPersistFoodItemToStorage()
+//    {
+//        System.out.println("persistFoodItemToStorage");
+//        
+//        String itemName = "Hvidløg";
+//        long itemAmount = 3;
+//        long itemPrice = 4444;
+//        
+//        assertEquals(2, facade.getFoodItemCount());
+//        assertEquals(1, facade.getStorageCount());
+//        
+//        facade.persistFoodItemToStorage(itemName, itemAmount, itemPrice);
+//        
+//        assertEquals(3, facade.getFoodItemCount());
+//        assertEquals(2, facade.getStorageCount());
+//    }
 
 }
