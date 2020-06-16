d37439325a75773929a3519f9a44d4da46c6fd7c
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-06-16 12:43:31 +0200

    Revert "Revert "Finish FoodItem entity""
    This reverts commit df355bb4cfb0702f29633e81d92bacc3f064bbf8.

--- src/main/java/entities/FoodItem.java
+++ src/main/java/entities/FoodItem.java
@@ -1,6 +1,8 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
@@ -28,7 +31,7 @@ public class FoodItem implements Serializable
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+//    @Column(name = "id")
     private Long id;
 
     @Column(nullable = false, unique = true, name = "item_name")
@@ -37,18 +40,26 @@ public class FoodItem implements Serializable
     @Column(name = "price_per_kg")
     private double pricePerKG;
 
+    // in which recipes is this ingredient used in
     @OneToMany(mappedBy = "foodItem", cascade =
     {
         CascadeType.MERGE, CascadeType.PERSIST
     })
-    private Set<Recipe_FoodItem> ingredientSet;
+    private Set<Recipe_FoodItem> recipes;
 
-    @ManyToOne
+//    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "storage_id")
     private Storage storage;
 
-    
-    
+    public FoodItem(String itemName, double pricePerKG)
+    {
+        this.itemName = itemName;
+        this.pricePerKG = pricePerKG;
+        this.recipes = new HashSet();
+        this.storage = null;
+    }
+
     public FoodItem()
     {
     }
@@ -61,6 +72,88 @@ public class FoodItem implements Serializable
     public void setId(Long id)
     {
         this.id = id;
+    }
+
+    public String getItemName()
+    {
+        return itemName;
+    }
+
+    public void setItemName(String itemName)
+    {
+        this.itemName = itemName;
+    }
+
+    public double getPricePerKG()
+    {
+        return pricePerKG;
+    }
+
+    public void setPricePerKG(double pricePerKG)
+    {
+        this.pricePerKG = pricePerKG;
+    }
+
+    public Set<Recipe_FoodItem> getRecipes()
+    {
+        return recipes;
+    }
+
+    public Storage getStorage()
+    {
+        return storage;
+    }
+
+    /**
+     * !!Warning!! This method should not be called! Use the setFoodItem method
+     * from the appropriate Storage object instead.
+     */
+    public void setStorage(Storage storage)
+    {
+        this.storage = storage;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final FoodItem other = (FoodItem) obj;
+        if (!Objects.equals(this.id, other.id))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "FoodItem{"
+                + "id=" + id
+                + ", itemName=" + itemName
+                + ", pricePerKG=" + pricePerKG
+                + ", recipes=" + recipes
+                + ", storage=" + storage
+                + '}';
     }
 
 }
